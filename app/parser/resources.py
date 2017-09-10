af92f71104609a03365dcb6959873602968731b1
Jed Davis <JDavis2759@gmail.com>
2017-09-09 19:37:24 -0500

    Progress on index view

--- app/parser/resources.py
+++ app/parser/resources.py
@@ -1,7 +1,9 @@
-from flask import jsonify
+from flask import jsonify, request
 from . import parser
 
 
 @parser.route('/')
 def index():
-    return jsonify(hello="world")
+    return jsonify(ipaddress=request.environ.get('REMOTE_ADDR'),
+                   language=request.accept_languages[0][0],
+                   software=request.user_agent.platform)
