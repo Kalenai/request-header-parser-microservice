080700b8c14b2d974e3d704ac51b7d1686849459
Jed Davis <JDavis2759@gmail.com>
2017-09-09 22:36:36 -0500

    Finished parser endpoint

--- app/parser/resources.py
+++ app/parser/resources.py
@@ -1,9 +1,14 @@
+import re
+from werkzeug import useragents
 from flask import jsonify, request
 from . import parser
 
 
 @parser.route('/')
 def index():
-    return jsonify(ipaddress=request.environ.get('REMOTE_ADDR'),
+    """
+    Returns a JSON response with the user's IP address, browser language, and operating system information.
+    """
+    return jsonify(ipaddress=request.remote_addr,
                    language=request.accept_languages[0][0],
-                   software=request.user_agent.platform)
+                   software=request.user_agent.string.split('(')[1].split(')')[0])
